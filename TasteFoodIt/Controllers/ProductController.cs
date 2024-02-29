fab249c3ca7559325f720c59a906c6125408b380
MuratYucedag <yucedagmurat23@gmail.com>
2024-02-29 21:20:13 +0300

    ...

--- TasteFoodIt/Controllers/ProductController.cs
+++ TasteFoodIt/Controllers/ProductController.cs
@@ -28,5 +28,37 @@ namespace TasteFoodIt.Controllers
             ViewBag.v = values;
             return View();
         } 
+
+        [HttpPost]
+        public ActionResult CreateProduct(Product p)
+        {
+            p.IsActive = true;
+            context.Products.Add(p);
+            context.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+
+        public ActionResult DeleteProduct(int id)
+        {
+            var value = context.Products.Find(id);
+            context.Products.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+        [HttpGet]
+        public ActionResult UpdateProduct(int id)
+        {
+            var value = context.Products.Find(id);
+
+            List<SelectListItem> categories = (from x in context.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+                                           }).ToList();
+            ViewBag.v = categories;
+
+            return View(value);
+        }
     }
 }
