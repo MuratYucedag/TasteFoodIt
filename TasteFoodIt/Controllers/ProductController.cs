61a7d4e51ed5b772fb93a025a9994cb72513f98f
MuratYucedag <yucedagmurat23@gmail.com>
2024-02-29 20:48:27 +0300

    ...

---
+++ TasteFoodIt/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasteFoodIt.Context;
+using TasteFoodIt.Entities;
+
+namespace TasteFoodIt.Controllers
+{
+    public class ProductController : Controller
+    {
+        TasteContext context = new TasteContext();
+        public ActionResult ProductList()
+        {
+            var values = context.Products.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateProduct()
+        {
+            List<SelectListItem> values = (from x in context.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
+            return View();
+        } 
+    }
+}
